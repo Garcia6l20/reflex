c2c54f1751f8b2e52da859e9fc1d0776420a7f7e
Sylvain Garcia <sylvain.garcia@tplsystemes.com>
2025-08-04 13:29:34 +0200

    reflex.utility: constant tuple support

--- lib/utility/include/reflex/constant.hpp
+++ lib/utility/include/reflex/constant.hpp
@@ -17,36 +17,73 @@ template <typename T> struct constant_wrapper;
 
 template <typename T> using constant_value_t = typename constant_wrapper<T>::value_type;
 
+template <class T>
+using constant_value_or_ref_t = [:is_lvalue_reference_type(^^T) ? ^^T
+                                                                : substitute(^^constant_value_t,
+                                                                             {
+                                                                                 ^^T}):];
+
 template <typename T>
   requires(is_structural_type(^^T))
 struct constant_wrapper<T>
 {
   using value_type = T;
+
+  constexpr static value_type wrap(value_type v)
+  {
+    return v;
+  }
 };
 
 namespace representation_object
 {
 template <class T, std::meta::info... Is> inline constexpr constant_value_t<T> array[] = {[:Is:]...};
 template <class T, std::meta::info... Is>
-inline constexpr constant_value_t<T> object = [] { return constant_wrapper<T>::unwrap(Is...); }();
+inline constexpr constant_value_t<T> object = []
+{
+  if constexpr(requires { constant_wrapper<T>::unwrap(Is...); })
+  {
+    return constant_wrapper<T>::unwrap(Is...);
+  }
+  else if constexpr(sizeof...(Is) == 1)
+  {
+    return extract<constant_value_t<T>>(Is...[0]);
+  }
+  else
+  {
+    std::unreachable();
+  }
+}();
 } // namespace representation_object
+
+template <typename T, typename... Infos> consteval decltype(auto) make_object(Infos... R)
+{
+  return extract<T const&>(object_of(substitute(^^representation_object::object,
+                                                {
+                                                    ^^T,
+                                                    R...})));
+}
 
 template <> struct constant_wrapper<std::string_view>
 {
   using value_type = std::string_view;
   static consteval decltype(auto) wrap(std::string_view const& s)
   {
-    return extract<value_type const&>(
-        object_of(substitute(^^representation_object::object,
-                             {
-                                 ^^value_type,
-                                 std::meta::reflect_constant(std::meta::reflect_constant_string(s))})));
+    return make_object<value_type>(std::meta::reflect_constant(std::meta::reflect_constant_string(s)));
   }
   static consteval value_type unwrap(std::meta::info r)
   {
-    return value_type(extract<char const*>(r));
-    // return value_type(extract<char const*>(r), extent(type_of(r)) - 1);
+    return value_type(extract<char const*>(r), extent(type_of(r)) - 1);
   }
+};
+template <> struct constant_wrapper<const char*> : constant_wrapper<std::string_view>
+{
+};
+template <size_t N> struct constant_wrapper<const char[N]> : constant_wrapper<std::string_view>
+{
+};
+template <> struct constant_wrapper<std::string> : constant_wrapper<std::string_view>
+{
 };
 
 template <std::ranges::input_range R> inline constexpr auto reflect_constant_array(R&& r)
@@ -68,16 +105,40 @@ struct constant_wrapper<R>
 
   static consteval value_type const& wrap(R const& v)
   {
-    return extract<value_type const&>(
-        object_of(substitute(^^representation_object::object,
-                             {
-                                 ^^R,
-                                 std::meta::reflect_constant(reflect_constant_array(v))})));
+    return make_object<value_type>(std::meta::reflect_constant(reflect_constant_array(v)));
   }
 
   static consteval value_type unwrap(std::meta::info r)
   {
     return value_type(extract<element_type const*>(r), extent(type_of(r)));
+  }
+};
+
+template <typename T> consteval decltype(auto) object_reflection(T&& obj)
+{
+  if constexpr(requires { reflect_constant(obj); })
+  {
+    return reflect_constant(reflect_constant(obj));
+  }
+  else
+  {
+    return reflect_constant(object_of(reflect_object(obj)));
+  }
+}
+
+template <typename... Ts> struct constant_wrapper<std::tuple<Ts...>>
+{
+  using value_type = std::tuple<constant_value_or_ref_t<Ts>...>;
+  static consteval value_type const& wrap(std::tuple<Ts...> const& v)
+  {
+    return std::apply(
+        [&](Ts const&... items) -> value_type const&
+        { return make_object<value_type>(object_reflection(constant_wrapper<std::decay_t<Ts>>::wrap(items))...); },
+        v);
+  }
+  template <typename... Infos> static consteval value_type unwrap(Infos... r)
+  {
+    return value_type(extract<constant_value_or_ref_t<Ts>>(r)...);
   }
 };
 
--- lib/utility/tests/test-constant.cpp
+++ lib/utility/tests/test-constant.cpp
@@ -3,95 +3,117 @@
 #include <reflex/testing_main.hpp>
 
 using namespace reflex;
+using namespace std::literals;
 
 namespace constant_tests
 {
-  namespace arithmetic_tests
+namespace arithmetic_tests
+{
+template <constant V> struct use_int_constant
+{
+  static constexpr int v = V;
+};
+void test()
+{
+  STATIC_CHECK_THAT(use_int_constant<42>::v == 42);
+}
+} // namespace arithmetic_tests
+namespace string_tests
+{
+template <constant V> struct use_string_constant
+{
+  static constexpr std::string_view v = V;
+};
+void test()
+{
+  constexpr auto i = use_string_constant<"hello"s>{};
+  std::println("s = {}", i.v);
+  STATIC_CHECK_THAT(i.v == "hello");
+}
+} // namespace string_tests
+namespace string_view_tests
+{
+template <constant V> struct use_string_constant
+{
+  static constexpr std::string_view v = V;
+};
+void test()
+{
+  STATIC_CHECK_THAT(use_string_constant<"hello"sv>::v == "hello");
+}
+} // namespace string_view_tests
+namespace string_literal_tests
+{
+template <constant V> struct use_string_constant
+{
+  static constexpr std::string_view v = V;
+};
+void test()
+{
+  use_string_constant<"hello"> c;
+  std::println("{}", c.v.size());
+  STATIC_CHECK_THAT(c.v == "hello");
   {
-    template <constant V> struct use_int_constant
-    {
-      static constexpr int v = V;
-    };
-    void test()
-    {
-      STATIC_CHECK_THAT(use_int_constant<42>::v == 42);
-    }
+    constexpr use_string_constant<"hello"sv> c2;
+    STATIC_CHECK_THAT(c2.v == "hello");
+    const void* p  = c.v.data();
+    const void* p2 = c2.v.data();
+    CHECK_THAT(p == p2);
   }
-  namespace string_tests
   {
-    template <constant V> struct use_string_constant
-    {
-      static constexpr std::string_view v = V;
-    };
-    void test()
-    {
-      using namespace std::literals;
-      constexpr auto i = use_string_constant<"hello"s>{};
-      std::println("s = {}", i.v);
-      STATIC_CHECK_THAT(i.v == "hello");
-    }
-  }
-  namespace string_view_tests
-  {
-    template <constant V> struct use_string_constant
-    {
-      static constexpr std::string_view v = V;
-    };
-    void test()
-    {
-      using namespace std::literals;
-      STATIC_CHECK_THAT(use_string_constant<"hello"sv>::v == "hello");
-    }
-  }
-  namespace string_literal_tests
-  {
-    template <constant V> struct use_string_constant
-    {
-      static constexpr std::string_view v = V;
-    };
-    void test()
-    {
-      using namespace std::literals;
-      use_string_constant<"hello"> c;
-      std::println("{}", c.v.size());
-      STATIC_CHECK_THAT(c.v == "hello");
-      {
-        constexpr use_string_constant<"hello"sv> c2;
-        STATIC_CHECK_THAT(c2.v == "hello");
-        const void *p = c.v.data();
-        const void *p2 = c2.v.data();
-        CHECK_THAT(p == p2);
-      }
-      {
-        constexpr use_string_constant<"hello"s> c2;
-        STATIC_CHECK_THAT(c2.v == "hello");
-        const void *p = c.v.data();
-        const void *p2 = c2.v.data();
-        CHECK_THAT(p == p2);
-      }
-    }
-  }
-  namespace vector_tests
-  {
-    template <constant V> struct use_vec
-    {
-      using value_type                              = typename std::decay_t<decltype(V)>::value_type;
-      static constexpr bool has_span_representation = template_of(^^value_type) == ^^std::span;
-      static constexpr auto v                       = V.get();
-    };
-    void test()
-    {
-      {
-        static constexpr auto with_vec = use_vec<std::vector{1, 2, 3}>{};
-        std::println("v = {}", with_vec.v);
-        STATIC_CHECK_THAT(std::ranges::equal(with_vec.v, std::array{1, 2, 3}));
-        STATIC_CHECK_THAT(with_vec.has_span_representation);
-      }
-      {
-        static constexpr auto with_array = use_vec<std::array{1, 2, 3}>{};
-        STATIC_CHECK_THAT(std::ranges::equal(with_array.v, std::array{1, 2, 3}));
-        STATIC_CHECK_THAT(not with_array.has_span_representation); // array uses structural representation
-      }
-    }
+    constexpr use_string_constant<"hello"s> c2;
+    STATIC_CHECK_THAT(c2.v == "hello");
+    const void* p  = c.v.data();
+    const void* p2 = c2.v.data();
+    CHECK_THAT(p == p2);
   }
 }
+} // namespace string_literal_tests
+namespace vector_tests
+{
+template <constant V> struct use_vec
+{
+  using value_type                              = typename std::decay_t<decltype(V)>::value_type;
+  static constexpr bool has_span_representation = template_of(^^value_type) == ^^std::span;
+  static constexpr auto v                       = V.get();
+};
+void test()
+{
+  {
+    static constexpr auto with_vec = use_vec<std::vector{1, 2, 3}>{};
+    std::println("v = {}", with_vec.v);
+    STATIC_CHECK_THAT(std::ranges::equal(with_vec.v, std::array{1, 2, 3}));
+    STATIC_CHECK_THAT(with_vec.has_span_representation);
+  }
+  {
+    static constexpr auto with_array = use_vec<std::array{1, 2, 3}>{};
+    STATIC_CHECK_THAT(std::ranges::equal(with_array.v, std::array{1, 2, 3}));
+    STATIC_CHECK_THAT(not with_array.has_span_representation); // array uses structural representation
+  }
+}
+} // namespace vector_tests
+namespace tuple_tests
+{
+template <constant V> struct use_tuple
+{
+  static constexpr auto v = V.get();
+};
+void test_ints()
+{
+  static constexpr auto v = use_tuple<std::tuple{42, 43}>{};
+  static_assert(std::get<0>(v.v) == 42);
+  static_assert(std::get<1>(v.v) == 43);
+}
+void test_strings()
+{
+  static constexpr auto v = use_tuple<std::tuple{42, "hello"}>{};
+  static_assert(std::get<0>(v.v) == 42);
+  static_assert(std::get<1>(v.v) == "hello");
+  // a unique "hello" constant shall be defined
+  const void* p  = std::get<1>(v.v).data();
+  const void* p2 = constant_string{"hello"}->data();
+  CHECK_THAT(p == p2);
+  CHECK_THAT(std::get<1>(v.v).data() == constant_string{"hello"}->data());
+}
+} // namespace tuple_tests
+} // namespace constant_tests
