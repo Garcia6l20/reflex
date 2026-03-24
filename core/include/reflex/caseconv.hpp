177757ea86d9412b2b0e245e33dcf19ccb108647
Sylvain Garcia <sylvain.garcia@tplsystemes.com>
2026-03-24 17:59:04 +0100

    caseconv: allow custom output type

--- core/include/reflex/caseconv.hpp
+++ core/include/reflex/caseconv.hpp
@@ -4,20 +4,27 @@
 
 namespace reflex::caseconv
 {
-constexpr std::string to_snake_case(std::string_view str)
+template <typename OutputT = std::string> constexpr OutputT to_snake_case(std::string_view str)
 {
-  std::string result;
-  result.reserve(str.size());
+  OutputT result;
+  if constexpr(requires { result.reserve(str.size()); })
+  {
+    result.reserve(str.size());
+  }
   for(std::size_t i = 0; i < str.size(); ++i)
   {
     char c = str[i];
     if(is_upper(c))
     {
-      if(i > 0 && is_lower(str[i - 1]))
+      if(i > 0 and is_lower(str[i - 1]))
       {
         result += '_';
       }
       result += to_lower(c);
+    }
+    else if(c == '-')
+    {
+      result += '_';
     }
     else
     {
@@ -26,10 +33,13 @@ constexpr std::string to_snake_case(std::string_view str)
   }
   return result;
 }
-constexpr std::string to_pascal_case(std::string_view str)
+template <typename OutputT = std::string> constexpr OutputT to_pascal_case(std::string_view str)
 {
-  std::string result;
-  result.reserve(str.size());
+  OutputT result;
+  if constexpr(requires { result.reserve(str.size()); })
+  {
+    result.reserve(str.size());
+  }
   bool capitalize_next = true;
   for(char c : str)
   {
@@ -52,9 +62,10 @@ constexpr std::string to_pascal_case(std::string_view str)
   }
   return result;
 }
-constexpr std::string to_kebab_case(std::string_view str)
+
+template <typename OutputT = std::string> constexpr OutputT to_kebab_case(std::string_view str)
 {
-  std::string result = to_snake_case(str);
+  OutputT result = to_snake_case<OutputT>(str);
   for(char& c : result)
   {
     if(c == '_')
@@ -64,9 +75,10 @@ constexpr std::string to_kebab_case(std::string_view str)
   }
   return result;
 }
-constexpr std::string to_camel_case(std::string_view str)
+
+template <typename OutputT = std::string> constexpr OutputT to_camel_case(std::string_view str)
 {
-  std::string result = to_pascal_case(str);
+  OutputT result = to_pascal_case<OutputT>(str);
   if(result.empty())
   {
     return result;
@@ -83,20 +95,20 @@ enum class naming
   pascal_case
 };
 
-constexpr std::string to_case(std::string_view str, naming n)
+template <typename OutputT = std::string> constexpr OutputT to_case(std::string_view str, naming n)
 {
   switch(n)
   {
     case naming::camel_case:
-      return to_camel_case(str);
+      return to_camel_case<OutputT>(str);
     case naming::snake_case:
-      return to_snake_case(str);
+      return to_snake_case<OutputT>(str);
     case naming::kebab_case:
-      return to_kebab_case(str);
+      return to_kebab_case<OutputT>(str);
     case naming::pascal_case:
-      return to_pascal_case(str);
+      return to_pascal_case<OutputT>(str);
   }
-  return std::string{str};
+  return OutputT{str};
 }
 
 } // namespace reflex::caseconv
